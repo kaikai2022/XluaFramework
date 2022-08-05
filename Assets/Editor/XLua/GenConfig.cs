82603efcf72ecbb314c0a73746e70eadb0b37736
kai <kai@kai>
2022-08-05 20:41:54 +0700

    更新 Gen

--- Assets/Editor/XLua/GenConfig.cs
+++ Assets/Editor/XLua/GenConfig.cs
@@ -1,205 +1,212 @@
-﻿using System.Collections.Generic;
-using System;
-using System.Reflection;
-using UnityEngine;
-using XLua;
-
-public static class GenConfig
+﻿public static class GenConfig
 {
     //lua中要使用到C#库的配置，比如C#标准库，或者Unity API，第三方库等。
-    [LuaCallCSharp] public static List<Type> LuaCallCSharp = new List<Type>()
-    {
-        // unity
-        typeof(System.Object),
-        typeof(UnityEngine.Object),
-        typeof(Ray2D),
-        typeof(GameObject),
-        typeof(Component),
-        typeof(Behaviour),
-        typeof(Transform),
-        typeof(Resources),
-        typeof(TextAsset),
-        typeof(Keyframe),
-        typeof(AnimationCurve),
-        typeof(AnimationClip),
-        typeof(MonoBehaviour),
-        typeof(ParticleSystem),
-        typeof(SkinnedMeshRenderer),
-        typeof(Renderer),
-        typeof(WWW),
-        typeof(UnityEngine.Networking.UnityWebRequest),
-        typeof(List<int>),
-        typeof(Action<string>),
-        typeof(UnityEngine.Debug),
-        typeof(Delegate),
-        typeof(Dictionary<string, GameObject>),
-        typeof(UnityEngine.Events.UnityEvent),
+    [XLua.LuaCallCSharp] public static System.Collections.Generic.List<System.Type> LuaCallCSharp =
+        new System.Collections.Generic.List<System.Type>()
+        {
+            // unity
+            typeof(System.Object),
+            typeof(UnityEngine.Object),
+            typeof(UnityEngine.Ray2D),
+            typeof(UnityEngine.GameObject),
+            typeof(UnityEngine.Component),
+            typeof(UnityEngine.Behaviour),
+            typeof(UnityEngine.Transform),
+            typeof(UnityEngine.Resources),
+            typeof(UnityEngine.TextAsset),
+            typeof(UnityEngine.Keyframe),
+            typeof(UnityEngine.AnimationCurve),
+            typeof(UnityEngine.AnimationClip),
+            typeof(UnityEngine.MonoBehaviour),
+            typeof(UnityEngine.ParticleSystem),
+            typeof(UnityEngine.SkinnedMeshRenderer),
+            typeof(UnityEngine.Renderer),
+            typeof(UnityEngine.WWW),
+            typeof(UnityEngine.Networking.UnityWebRequest),
+            typeof(System.Collections.Generic.List<int>),
+            typeof(System.Action<string>),
+            typeof(UnityEngine.Debug),
+            typeof(System.Delegate),
+            typeof(System.Collections.Generic.Dictionary<string, UnityEngine.GameObject>),
+            typeof(UnityEngine.Events.UnityEvent),
 
-        // unity结合lua，这部分导出很多功能在lua侧重新实现，没有实现的功能才会跑到cs侧
-        typeof(Bounds),
-        typeof(Color),
-        typeof(LayerMask),
-        typeof(Mathf),
-        typeof(Plane),
-        typeof(Quaternion),
-        typeof(Ray),
-        typeof(RaycastHit),
-        typeof(Time),
-        typeof(Touch),
-        typeof(TouchPhase),
-        typeof(Vector2),
-        typeof(Vector3),
-        typeof(Vector4),
+#if PLATFORM_ANDROID || UNITY_ANDROID
+            typeof(UnityEngine.Android.Permission),
+#endif
 
-        // 渲染
-        typeof(RenderMode),
+            // unity结合lua，这部分导出很多功能在lua侧重新实现，没有实现的功能才会跑到cs侧
+            typeof(UnityEngine.Bounds),
+            typeof(UnityEngine.Color),
+            typeof(UnityEngine.LayerMask),
+            typeof(UnityEngine.Mathf),
+            typeof(UnityEngine.Plane),
+            typeof(UnityEngine.Quaternion),
+            typeof(UnityEngine.Ray),
+            typeof(UnityEngine.RaycastHit),
+            typeof(UnityEngine.Time),
+            typeof(UnityEngine.Touch),
+            typeof(UnityEngine.TouchPhase),
+            typeof(UnityEngine.Vector2),
+            typeof(UnityEngine.Vector3),
+            typeof(UnityEngine.Vector4),
 
-        // UGUI  
-        typeof(UnityEngine.Canvas),
-        typeof(UnityEngine.Rect),
-        typeof(UnityEngine.RectTransform),
-        typeof(UnityEngine.RectOffset),
-        typeof(UnityEngine.Sprite),
-        typeof(UnityEngine.UI.CanvasScaler),
-        typeof(UnityEngine.UI.CanvasScaler.ScaleMode),
-        typeof(UnityEngine.UI.CanvasScaler.ScreenMatchMode),
-        typeof(UnityEngine.UI.GraphicRaycaster),
-        typeof(UnityEngine.UI.Text),
-        typeof(UnityEngine.UI.InputField),
-        typeof(UnityEngine.UI.Button),
-        typeof(UnityEngine.UI.Image),
-        typeof(UnityEngine.UI.ScrollRect),
-        typeof(UnityEngine.UI.Scrollbar),
-        typeof(UnityEngine.UI.Toggle),
-        typeof(UnityEngine.UI.ToggleGroup),
-        typeof(UnityEngine.UI.Button.ButtonClickedEvent),
-        typeof(UnityEngine.UI.ScrollRect.ScrollRectEvent),
-        typeof(UnityEngine.UI.GridLayoutGroup),
-        typeof(UnityEngine.UI.ContentSizeFitter),
-        typeof(UnityEngine.UI.Slider),
-        typeof(UIPointerClick),
-        typeof(UIPointerDoubleClick),
-        typeof(UIPointerDownUp),
-        typeof(UIPointerLongPress),
+            // 渲染
+            typeof(UnityEngine.RenderMode),
 
-        // easy touch
-        // TODO：后续需要什么脚本再添加进来
-        typeof(ETCArea),
-        typeof(ETCAxis),
-        typeof(ETCButton),
-        typeof(ETCInput),
-        typeof(ETCJoystick),
+            // UGUI  
+            typeof(UnityEngine.Canvas),
+            typeof(UnityEngine.Rect),
+            typeof(UnityEngine.RectTransform),
+            typeof(UnityEngine.RectOffset),
+            typeof(UnityEngine.Sprite),
+            typeof(UnityEngine.UI.CanvasScaler),
+            typeof(UnityEngine.UI.CanvasScaler.ScaleMode),
+            typeof(UnityEngine.UI.CanvasScaler.ScreenMatchMode),
+            typeof(UnityEngine.UI.GraphicRaycaster),
+            typeof(UnityEngine.UI.Text),
+            typeof(UnityEngine.UI.InputField),
+            typeof(UnityEngine.UI.Button),
+            typeof(UnityEngine.UI.Image),
+            typeof(UnityEngine.UI.ScrollRect),
+            typeof(UnityEngine.UI.Scrollbar),
+            typeof(UnityEngine.UI.Toggle),
+            typeof(UnityEngine.UI.ToggleGroup),
+            typeof(UnityEngine.UI.Button.ButtonClickedEvent),
+            typeof(UnityEngine.UI.ScrollRect.ScrollRectEvent),
+            typeof(UnityEngine.UI.GridLayoutGroup),
+            typeof(UnityEngine.UI.ContentSizeFitter),
+            typeof(UnityEngine.UI.Slider),
+            typeof(UIPointerClick),
+            typeof(UIPointerDoubleClick),
+            typeof(UIPointerDownUp),
+            typeof(UIPointerLongPress),
 
-        // 场景、资源加载
-        typeof(UnityEngine.Resources),
-        typeof(UnityEngine.ResourceRequest),
-        typeof(UnityEngine.SceneManagement.SceneManager),
-        typeof(AsyncOperation),
+            typeof(UnityEngine.U2D.SpriteAtlas),
 
-        // 其它
-        typeof(PlayerPrefs),
-        typeof(System.GC),
-    };
+            // easy touch
+            // TODO：后续需要什么脚本再添加进来
+            typeof(ETCArea),
+            typeof(ETCAxis),
+            typeof(ETCButton),
+            typeof(ETCInput),
+            typeof(ETCJoystick),
+
+            // 场景、资源加载
+            typeof(UnityEngine.Resources),
+            typeof(UnityEngine.ResourceRequest),
+            typeof(UnityEngine.SceneManagement.SceneManager),
+            typeof(UnityEngine.AsyncOperation),
+
+            // 其它
+            typeof(UnityEngine.PlayerPrefs),
+            typeof(System.GC),
+        };
 
     //C#静态调用Lua的配置（包括事件的原型），仅可以配delegate，interface
-    [CSharpCallLua] public static List<Type> CSharpCallLua = new List<Type>()
-    {
-        // unity
-        typeof(Action),
-        typeof(Action<int>),
-        typeof(Action<WWW>),
-        typeof(Callback),
-        typeof(UnityEngine.Event),
-        typeof(UnityEngine.Events.UnityAction),
-        typeof(System.Collections.IEnumerator),
-        typeof(UnityEngine.Events.UnityAction<Vector2>),
-    };
+    [XLua.CSharpCallLua] public static System.Collections.Generic.List<System.Type> CSharpCallLua =
+        new System.Collections.Generic.List<System.Type>()
+        {
+            // unity
+            typeof(System.Action),
+            typeof(System.Action<int>),
+            typeof(System.Action<UnityEngine.WWW>),
+            typeof(Callback),
+            typeof(UnityEngine.Event),
+            typeof(UnityEngine.Events.UnityAction),
+            typeof(System.Collections.IEnumerator),
+            typeof(UnityEngine.Events.UnityAction<UnityEngine.Vector2>),
+        };
 
     //黑名单
-    [BlackList] public static List<List<string>> BlackList = new List<List<string>>()
-    {
-        new List<string>() {"System.Xml.XmlNodeList", "ItemOf"},
-        new List<string>() {"UnityEngine.WWW", "movie"},
-        new List<string>() {"UnityEngine.UI.Text", "OnRebuildRequested"},
+    [XLua.BlackList] public static System.Collections.Generic.List<System.Collections.Generic.List<string>> BlackList =
+        new System.Collections.Generic.List<System.Collections.Generic.List<string>>()
+        {
+            new System.Collections.Generic.List<string>() {"System.Xml.XmlNodeList", "ItemOf"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.WWW", "movie"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.UI.Text", "OnRebuildRequested"},
 #if UNITY_WEBGL
-        new List<string>() {"UnityEngine.WWW", "threadPriority"},
-        new List<string>() {"UnityEngine.Light", "SetLightDirty"},
-        new List<string>() {"UnityEngine.Light", "shadowRadius"},
-        new List<string>() {"UnityEngine.Light", "shadowAngle"},
-        new List<string>() {"System.Activator", "CreateInstance", "System.String", "System.String"},
-        new List<string>() {"System.Activator", "CreateInstance", "System.ActivationContext"},
-        new List<string>() {"System.Activator", "CreateInstance", "System.ActivationContext", "System.String[]"},
-        new List<string>() {"System.Activator", "CreateInstance", "System.String", "System.String", "System.Object[]"},
-        new List<string>() {"System.Activator", "CreateInstance", "System.AppDomain", "System.String", "System.String"},
-        new List<string>()
+        new System.Collections.Generic.List<string>() {"UnityEngine.WWW", "threadPriority"},
+        new System.Collections.Generic.List<string>() {"UnityEngine.Light", "SetLightDirty"},
+        new System.Collections.Generic.List<string>() {"UnityEngine.Light", "shadowRadius"},
+        new System.Collections.Generic.List<string>() {"UnityEngine.Light", "shadowAngle"},
+        new System.Collections.Generic.List<string>() {"System.Activator", "CreateInstance", "System.String", "System.String"},
+        new System.Collections.Generic.List<string>() {"System.Activator", "CreateInstance", "System.ActivationContext"},
+        new System.Collections.Generic.List<string>() {"System.Activator", "CreateInstance", "System.ActivationContext", "System.String[]"},
+        new System.Collections.Generic.List<string>() {"System.Activator", "CreateInstance", "System.String", "System.String", "System.Object[]"},
+        new System.Collections.Generic.List<string>() {"System.Activator", "CreateInstance", "System.AppDomain", "System.String", "System.String"},
+        new System.Collections.Generic.List<string>()
         {
             "System.Activator", "CreateInstance", "System.AppDomain", "System.String", "System.String",
             "System.Boolean",
             "System.Reflection.BindingFlags", "System.Reflection.Binder", "System.Object[]",
             "System.Globalization.CultureInfo", "System.Object[]"
         },
-        new List<string>()
+        new System.Collections.Generic.List<string>()
         {
             "System.Activator", "CreateInstance", "System.String", "System.String", "System.Boolean",
             "System.Reflection.BindingFlags", "System.Reflection.Binder", "System.Object[]",
             "System.Globalization.CultureInfo", "System.Object[]"
         },
-        new List<string>() {"System.Activator", "CreateInstanceFrom", "System.String", "System.String"},
-        new List<string>()
+        new System.Collections.Generic.List<string>() {"System.Activator", "CreateInstanceFrom", "System.String", "System.String"},
+        new System.Collections.Generic.List<string>()
             {"System.Activator", "CreateInstanceFrom", "System.String", "System.String", "System.Object[]"},
-        new List<string>()
+        new System.Collections.Generic.List<string>()
             {"System.Activator", "CreateInstanceFrom", "System.AppDomain", "System.String", "System.String"},
-        new List<string>()
+        new System.Collections.Generic.List<string>()
         {
             "System.Activator", "CreateInstanceFrom", "System.AppDomain", "System.String", "System.String",
             "System.Boolean",
             "System.Reflection.BindingFlags", "System.Reflection.Binder", "System.Object[]",
             "System.Globalization.CultureInfo", "System.Object[]"
         },
-        new List<string>()
+        new System.Collections.Generic.List<string>()
         {
             "System.Activator", "CreateInstanceFrom", "System.String", "System.String", "System.Boolean",
             "System.Reflection.BindingFlags", "System.Reflection.Binder", "System.Object[]",
             "System.Globalization.CultureInfo", "System.Object[]"
         },
-        new List<string>() {"System.Activator", "CreateComInstanceFrom", "System.String", "System.String"},
-        new List<string>()
+        new System.Collections.Generic.List<string>() {"System.Activator", "CreateComInstanceFrom", "System.String", "System.String"},
+        new System.Collections.Generic.List<string>()
             {"System.Activator", "CreateComInstanceFrom", "System.String", "System.String", "System.Byte[]","System.Configuration.Assemblies.AssemblyHashAlgorithm"},
-        new List<string>() {"System.Activator", "GetObject", "System.Type", "System.String"},
-        new List<string>() {"System.Activator", "GetObject", "System.Type", "System.String", "System.Object"},
+        new System.Collections.Generic.List<string>() {"System.Activator", "GetObject", "System.Type", "System.String"},
+        new System.Collections.Generic.List<string>() {"System.Activator", "GetObject", "System.Type", "System.String", "System.Object"},
 
 
 #endif
-        new List<string>() {"UnityEngine.Texture2D", "alphaIsTransparency"},
-        new List<string>() {"UnityEngine.Security", "GetChainOfTrustValue"},
-        new List<string>() {"UnityEngine.CanvasRenderer", "onRequestRebuild"},
-        new List<string>() {"UnityEngine.Light", "areaSize"},
-        new List<string>() {"UnityEngine.Light", "lightmapBakeType"},
-        new List<string>() {"UnityEngine.WWW", "MovieTexture"},
-        new List<string>() {"UnityEngine.WWW", "GetMovieTexture"},
-        new List<string>() {"UnityEngine.AnimatorOverrideController", "PerformOverrideClipListCleanup"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.Texture2D", "alphaIsTransparency"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.Security", "GetChainOfTrustValue"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.CanvasRenderer", "onRequestRebuild"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.Light", "areaSize"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.Light", "lightmapBakeType"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.WWW", "MovieTexture"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.WWW", "GetMovieTexture"},
+            new System.Collections.Generic.List<string>()
+                {"UnityEngine.AnimatorOverrideController", "PerformOverrideClipListCleanup"},
 #if !UNITY_WEBPLAYER
-        new List<string>() {"UnityEngine.Application", "ExternalEval"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.Application", "ExternalEval"},
 #endif
-        new List<string>() {"UnityEngine.GameObject", "networkView"}, //4.6.2 not support
-        new List<string>() {"UnityEngine.Component", "networkView"}, //4.6.2 not support
-        new List<string>()
-            {"System.IO.FileInfo", "GetAccessControl", "System.Security.AccessControl.AccessControlSections"},
-        new List<string>() {"System.IO.FileInfo", "SetAccessControl", "System.Security.AccessControl.FileSecurity"},
-        new List<string>()
-            {"System.IO.DirectoryInfo", "GetAccessControl", "System.Security.AccessControl.AccessControlSections"},
-        new List<string>()
-            {"System.IO.DirectoryInfo", "SetAccessControl", "System.Security.AccessControl.DirectorySecurity"},
-        new List<string>()
-        {
-            "System.IO.DirectoryInfo", "CreateSubdirectory", "System.String",
-            "System.Security.AccessControl.DirectorySecurity"
-        },
-        new List<string>() {"System.IO.DirectoryInfo", "Create", "System.Security.AccessControl.DirectorySecurity"},
-        new List<string>() {"UnityEngine.MonoBehaviour", "runInEditMode"},
-        new List<string>() {"System.Type", "MakeGenericSignatureType", "System.Type", "System.Type[]"},
-        new List<string>() {"System.Type", "IsCollectible"},
-    };
+            new System.Collections.Generic.List<string>() {"UnityEngine.GameObject", "networkView"}, //4.6.2 not support
+            new System.Collections.Generic.List<string>() {"UnityEngine.Component", "networkView"}, //4.6.2 not support
+            new System.Collections.Generic.List<string>()
+                {"System.IO.FileInfo", "GetAccessControl", "System.Security.AccessControl.AccessControlSections"},
+            new System.Collections.Generic.List<string>()
+                {"System.IO.FileInfo", "SetAccessControl", "System.Security.AccessControl.FileSecurity"},
+            new System.Collections.Generic.List<string>()
+                {"System.IO.DirectoryInfo", "GetAccessControl", "System.Security.AccessControl.AccessControlSections"},
+            new System.Collections.Generic.List<string>()
+                {"System.IO.DirectoryInfo", "SetAccessControl", "System.Security.AccessControl.DirectorySecurity"},
+            new System.Collections.Generic.List<string>()
+            {
+                "System.IO.DirectoryInfo", "CreateSubdirectory", "System.String",
+                "System.Security.AccessControl.DirectorySecurity"
+            },
+            new System.Collections.Generic.List<string>()
+                {"System.IO.DirectoryInfo", "Create", "System.Security.AccessControl.DirectorySecurity"},
+            new System.Collections.Generic.List<string>() {"UnityEngine.MonoBehaviour", "runInEditMode"},
+            new System.Collections.Generic.List<string>()
+                {"System.Type", "MakeGenericSignatureType", "System.Type", "System.Type[]"},
+            new System.Collections.Generic.List<string>() {"System.Type", "IsCollectible"},
+        };
 
 // #if UNITY_2018_1_OR_NEWER
 //     [BlackList]
